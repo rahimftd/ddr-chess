df8f95702feeb24c7acb0a2aed5966b672684df1
Rahim <rahim.dharssi@gmail.com>
2016-02-06 00:13:26 -0800

    started implementing search algo

--- frontend/rahim.js
+++ frontend/rahim.js
@@ -1,8 +1,8 @@
-var lastMove = {
+/*var lastMove = {
   from: "",
   to: "",
   dests: ""
-};
+};*/
 
 var moveHistory = [];
 
@@ -16,11 +16,13 @@ var pieceValue = {
 
 function movePiece(f, t) {
   moveHistory.push({
-    
-  })
-  lastMove.from = f;
+    from: f,
+    to: t,
+    dests: getValidMoves()
+  });
+  /*lastMove.from = f;
   lastMove.to = t;
-  lastMove.dests = getValidMoves();
+  lastMove.dests = getValidMoves();*/
   ground.move(f, t);
   chess.move({from: f, to: t});
   ground.set({
@@ -34,6 +36,7 @@ function movePiece(f, t) {
 
 function reverseMove() {
   var newTurnColor = switchTurnColor(getTurnColor());
+  var lastMove = moveHistory.pop();
   ground.move(lastMove.to, lastMove.from);
   chess.move({from:lastMove.to, to:lastMove.from});
   ground.set({
@@ -65,16 +68,19 @@ function evaluatePosition() {
   function sumMaterial() {
     var piecesOnBoard = ground.getPieces();
     var material = 0;
+    var opponentMaterial = 0;
 
     for (var piece in piecesOnBoard) {
       if (piece.color === getTurnColor) {
         material += pieceValue[piece.role];
+      } else {
+        opponentMaterial += pieceValue[piece.role];
       }
     }
-    return material;
+    return material - opponentMaterial;
   }
 
-  function getMovability() {
+  /*function getMovability() {
     var availableMoves = 0;
 
     for (var piece in getValidMoves()) {
@@ -84,7 +90,71 @@ function evaluatePosition() {
 
   var movabilityScore = getMovability();
   var materialScore = sumMaterial();
-  var pawnScore = 0;
+  var pawnScore = 0;*/
 
-  return 0.1 * movabilityScore + materialScore - 0.5 * pawnScore;
+  return sumMaterial();
+}
+
+function findBestMove(searchDepth, nodeType, atRoot) {
+  var bestMove = {
+    from: "",
+    to: "",
+    value: 0
+  };
+
+  var nodeValue;
+
+  var validMoves = destsToArray();
+
+  if (searchDepth === 0) {
+    return evaluatePosition();
+  } else if (nodeType === "max") {
+    
+  }
+}
+
+function getMinNodeValue() {
+  var validMoves = destsToArray();
+  var minNodeValue = 0;
+  var bestMove = {};
+
+  for (var i = 0; i < validMoves.length; i++) {
+    movePiece(validMoves[i].from, validMoves[i].to);
+    if (evaluatePosition() < minNodeValue || minNodeValue === 0) {
+      minNodeValue = evaluatePosition();
+      bestMove = validMoves[i];
+    }
+    reverseMove();
+  }
+
+  return minNodeValue;
+}
+
+function getMaxNodeValue() {
+  var validMoves = destsToArray();
+  var maxNodeValue = 0;
+  var bestMove = {};
+
+  for (var i = 0; i < validMoves.length; i++) {
+    movePiece(validMoves[i].from, validMoves[i].to);
+    if (evaluatePosition() > maxNodeValue || maxNodeValue === 0) {
+      maxNodeValue = evaluatePosition();
+      bestMove = validMoves[i];
+    }
+    reverseMove();
+  }
+
+  return maxNodeValue;
+}
+
+function destsToArray() {
+  var dests = getValidMoves();
+  var movesArray = [];
+
+  for (var move in dests) {
+    for (var i = 0; i < dests[move].length; i++) {
+      movesArray.push({from: move, to: dests[move][i]});
+    }
+  }
+  return movesArray;
 }
